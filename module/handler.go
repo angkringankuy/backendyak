8ef7c2f5c72e7935e11a8747545de3a70746d507
“MSyahidAlFajri” <masyahida4@gmail.com>
2023-11-07 02:13:51 +0700

    Update InsertTransaksi

--- module/handler.go
+++ module/handler.go
@@ -142,7 +142,7 @@ func Transaksi(Mongoenv, dbname string, r *http.Request) string {
         resp.Message = "error parsing application/json: " + err.Error()
     } else {
         resp.Status = true
-        insertedID, err := InsertPayment(conn, "help", data.Email, data.OrderID)
+        insertedID, err := InsertPayment(conn, "transaksi", data.Email, data.OrderID)
         if err != nil {
             resp.Message = "Gagal memasukkan data ke database: " + err.Error()
         } else {
